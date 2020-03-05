142a5d58f16e4325700c01a26666f3f5a206028a
albrdev <albrdev@gmail.com>
2020-03-05 14:40:50 +0000

    fixed syntax

--- database/init_user.sql
+++ database/init_user.sql
@@ -1,4 +1,3 @@
 CREATE USER 'rise'@'localhost' IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON 'rise'.* TO 'rise'@'localhost';
+GRANT ALL PRIVILEGES ON `rise`.* TO 'rise'@'localhost';
 FLUSH PRIVILEGES;
-
