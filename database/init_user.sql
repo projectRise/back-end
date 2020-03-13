6985bd57feb99b6bfa9f4479d10dcf5be7b8f87d
albrdev <albrdev@gmail.com>
2020-03-13 08:34:07 +0000

    added remote access

--- database/init_user.sql
+++ database/init_user.sql
@@ -1,3 +1,7 @@
 CREATE USER 'rise'@'localhost' IDENTIFIED BY 'password';
 GRANT ALL PRIVILEGES ON `rise`.* TO 'rise'@'localhost';
+
+CREATE USER 'rise'@'%' IDENTIFIED BY 'password';
+GRANT SELECT ON `rise`.* TO 'rise'@'%';
+
 FLUSH PRIVILEGES;
