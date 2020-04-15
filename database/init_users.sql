95fc262f62fca6869cf497b1787a9a625198e46f
albrdev <albrdev@gmail.com>
2020-04-15 15:11:43 +0200

    renamed file

--- database/init_users.sql
+++ database/init_users.sql
@@ -1,7 +1,7 @@
-CREATE USER 'rise'@'localhost' IDENTIFIED BY 'password';
+CREATE USER 'rise'@'localhost' IDENTIFIED BY @`password`;
 GRANT ALL PRIVILEGES ON `rise`.* TO 'rise'@'localhost';
 
-CREATE USER 'rise'@'%' IDENTIFIED BY 'password';
+CREATE USER 'rise'@'%' IDENTIFIED BY @`password`;
 GRANT SELECT ON `rise`.* TO 'rise'@'%';
 
 FLUSH PRIVILEGES;
